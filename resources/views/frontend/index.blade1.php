d87a06b2f5e6ceccdcd7707dce859b577fb0b9a4
mohan <mohan@dotcomweavers.com>
2017-03-30 15:25:29 +0530

    changes

--- resources/views/frontend/index.blade1.php
+++ resources/views/frontend/index.blade1.php
@@ -14,36 +14,46 @@
 @endsection
 
 @section('content')
-	<!-- responsive banner start here -->
-		<div class="container  hidden-sm  hidden-md hidden-lg">
-			<div id="main-banner" class="carousel slide" data-ride="carousel">
-				<!-- Indicators -->
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-				</ol>
-				<!-- Wrapper for slides -->
-				<div class="carousel-inner" role="listbox">
-					<div class="item active">
-						<img class="img-responsive" src="{{asset('/assets/frontend/img/bnr-11.png')}}">
-					</div>
-					<div class="item">
-						<img class="img-responsive" src="{{asset('/assets/frontend/img/bnr-22.png')}}">
-					</div>
-					<div class="item">
-						<img class="img-responsive" src="{{asset('/assets/frontend/img/bnr-33.png')}}">
-					</div>
-				</div>
-				<!-- Left and right controls -->
-				<a class="left carousel-control" href="#main-banner" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#main-banner" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
+	<section class="main_banner">
+    <div class="container">
+	<div class="row">
+	<div class="col-md-8 col-sm-8 col-xs-12 jumb-bnr">
+	<a href="#"><img class="img-responsive" src="../assets/frontend/img/bnr-11.png"></a>
+	</div>
+	<div class="col-md-4 col-sm-4 col-xs-12 jumb-smll">
+	<a href="#"><img class="img-responsive" src="../assets/frontend/img/bnr-22.png"></a>
+		<a href="#"><img class="img-responsive" src="../assets/frontend/img/bnr-33.png"></a>
+	</div>
+	</div>
+	<div class="container">
+	<div class="row home_steps">
+		<div class="col-md-4">
+		<div class="media"> 
+			<div class="media-left"> <a href="#"> <img alt="64x64" class="media-object" data-src="holder.js/64x64" src="../assets/frontend/img/step_1_icon.png" data-holder-rendered="true" > </a> 
+			</div> 
+			<div class="media-body"> <h4 class="media-heading">STEP 1</h4>
+			<p>Sell those old costumes with Chrysalis</p> 
+			</div> 
+		</div>
+			</div>
+				<div class="col-md-4">
+		<div class="media"> 
+			<div class="media-left"> <a href="#"> <img alt="64x64" class="media-object" data-src="holder.js/64x64" src="../assets/frontend/img/step_2_icon.png" data-holder-rendered="true" > </a> 
+			</div> 
+			<div class="media-body"> <h4 class="media-heading">STEP 2</h4>
+			<p>Get Cash or Store Credit</p> 
+			</div> 
+		</div>
+			</div>
+				<div class="col-md-4">
+		<div class="media"> 
+			<div class="media-left"> <a href="#"> <img alt="64x64" class="media-object" data-src="holder.js/64x64" src="../assets/frontend/img/step_3_icon.png" data-holder-rendered="true" > </a> 
+			</div> 
+			<div class="media-body"> <h4 class="media-heading">STEP 3</h4>
+			<p>Buy another costume!</p> 
+			</div> 
+		</div>
+
 			</div>
 		</div>
 		<!-- responsive banner End here -->
