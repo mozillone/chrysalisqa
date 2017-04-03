e6c12f4c0693fe9a29fdcc3e02fc8642415e4981
V Sudhamsh Mohan <mohan@dotcomweavers.com>
2017-04-03 17:47:37 +0530

    changes

--- public_html/assets/frontend/js/pages/costumes_listing.js
+++ public_html/assets/frontend/js/pages/costumes_listing.js
@@ -19,7 +19,7 @@ function pagination()
 {
   $("ul.holder").jPages({
         containerID  : "itemContainer",
-        perPage      : 4,
+        perPage      : 12,
         startPage    : 1,
         startRange   : 1,
         midRange     : 5,
