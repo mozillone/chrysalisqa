fd0bb9922f27d88d2dfcd2171c66fec3b0fb03c6
Mruduramai <mgurrapu@dotcomweavers.com>
2017-04-21 19:28:32 +0530

    changes

--- public_html/assets/admin/js/pages/customers.js
+++ public_html/assets/admin/js/pages/customers.js
@@ -234,6 +234,9 @@ $(function(){
 				avatar1:{
 				  required: true,
 				},
+				avatar2:{
+				  required: true,
+				},
 			},
 			highlight: function(element) {
           	 $(element).closest('.form-control').addClass('error');
@@ -428,13 +431,74 @@ $(function(){
 		   	$('input[name="avatar1"]').val("1");
 			}
 		 }); 
-	$(".remove_pic").on("click",function(){
+		 $("#profile_logo2").on('change', function(){
+		//Get count of selected files
+		var countFiles = $(this)[0].files.length;
+		var imgPath = $(this)[0].value;
+		var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+		var image_holder = $("#img-chan2");
+		image_holder.empty();
+		var size = parseFloat($("#profile_logo2")[0].files[0].size / 1024).toFixed(2);
+		if (extn == "jpg" || extn == "jpeg" || extn == "png") {
+			if(size<10000)
+            {
+			if (typeof(FileReader) != "undefined") {
+			
+				for (var i = 0; i < countFiles; i++) 
+				{
+					var reader = new FileReader();
+					reader.onload = function(e) {
+						
+						$('#img-chan2').attr('src',e.target.result);
+					}
+					image_holder.show();
+					reader.readAsDataURL($(this)[0].files[i]);
+				}
+				} else {
+				swal("This browser does not support FileReader.");
+			}
+			
+            } else {
+
+                  
+                    $("#profile_logo2").val("");
+                    swal({   
+                        title: "File doesn't Support",   
+                        text: "Upload Below 10MB Size Only",   
+                        type: "warning",   
+                        showCancelButton: false,
+                        fieldset:false,
+                        confirmButtonColor: "#DD6B55 ",   
+                        confirmButtonText: "Ok",   
+                    closeOnConfirm: true });
+                }
+			} else {
+			//swal("");
+			swal({   
+				title: "File doesn't Support",   
+				text: "Upload .JPG, .JPEG, .PNG Images only.!",   
+				type: "warning",   
+				showCancelButton: false,
+				fieldset:false,
+				confirmButtonColor: "#DD6B55",   
+				confirmButtonText: "Ok",   
+			closeOnConfirm: true });
+			$('input[type="file"]').val('');
+		   	$('input[name="avatar2"]').val("1");
+			}
+		 }); 
+		$(".remove_pic").on("click",function(){
 		$('#img-chan').attr('src',"/img/default.png");
 		$('input[type="file"]').val('');
 		$('input[name="is_removed"]').val("1");
 	  });
 	  $(".remove_pic1").on("click",function(){
 		$('#img-chan1').attr('src',"/img/default.png");
+		$('input[type="file"]').val('');
+		$('input[name="is_removed"]').val("1");
+	  });
+	  $(".remove_pic2").on("click",function(){
+		$('#img-chan2').attr('src',"/img/default.png");
 		$('input[type="file"]').val('');
 		$('input[name="is_removed"]').val("1");
 	  });
