ac209490c9b3730d257f8a84aa6f6ee904719d83
admin <jithender@dotcomweavers.com>
2017-05-05 22:10:57 +0530

    checkout process

--- app/Creditcard.php
+++ app/Creditcard.php
@@ -20,20 +20,35 @@ class Creditcard extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'user_id', 'display_name', 'cardholder_name', 'credit_card_mask', 'card_type', 'is_default', 'payment_method_token'
+        'id', 'user_id', 'display_name', 'cardholder_name', 'credit_card_mask', 'card_type', 'is_default', 'payment_method_token','exp_month','exp_year','cvn_pin',
     ];
 
-   protected function createCC($req){
+   protected function addCreditCard($req,$user_id){
            
         $cc_details=array('user_id'=>$user_id,
                        'cardholder_name'=>$req['cardholder_name'],
-                       'credit_card_mask'=> $req['credit_card_mask'],
+                       'credit_card_mask'=> $req['cc_number'],
+                       'exp_month'=> $req['exp_month'],
+                       'exp_year'=> $req['exp_year'],
+                       'cvn_pin'=> $req['cvn_pin'],
                        'created_at'=>date('Y-m-d H:i:s'));
-        Site_model::insert_get_id('creditcard',$cc_details);
-        return true;
+        $cc_id=Site_model::insert_get_id('creditcard',$cc_details);
+         $this->updateCartOrderInfo($cc_id,$req['cart_id']);
+        return $cc_id;
         }
-    protected function getCCList($user_id){
-        $cc_list=DB::Select('select * from cc_creditcard where user_id="'.$user_id.'"');
+    protected function getCCList($user_id,$cc_id=null){
+        if($cc_id==null){
+            $where='where user_id="'.$user_id.'"';
+        }else{
+            $where='where user_id="'.$user_id.'" and id='.$cc_id.'';
+        }
+        $cc_list=DB::Select('select * from cc_creditcard '.$where.'');
         return $cc_list;
-    }    
+    }  
+    private function updateCartOrderInfo($cc_id,$cart_id){
+          $data=array('cc_id'=>$cc_id);
+          $cond=array('cart_id'=>$cart_id);
+          Site_model::update_data('cart',$data,$cond);
+          return true;    
+     }  
 }
