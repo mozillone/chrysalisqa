32d02330ec759b26af3758db12235a1a06141435
Mruduramai <mgurrapu@dotcomweavers.com>
2017-05-11 17:31:36 +0530

    changes

--- resources/views/admin/costumes/costumes_list.blade.php
+++ resources/views/admin/costumes/costumes_list.blade.php
@@ -96,6 +96,15 @@
     </div>
 </section>
 
+@stop
+
+
+{{-- page level scripts --}}
+@section('footer_scripts') 
+<script src="{{ asset('angular/Admin/UserManagement/Controllers/users-lists.js') }}"></script>
+<script src="{{ asset('angular/Admin/UserManagement/Services/user_management.js') }}"></script>
+<script src="{{ asset('/vendors/sweetalert/dist/sweetalert.min.js')}}"></script>
+
 
 <script type="text/javascript">
    function changeCostumeStatus(id, status) {
@@ -157,65 +166,6 @@
 
 
    }
-
-</script>
-@stop
-
-
-{{-- page level scripts --}}
-@section('footer_scripts') 
-<script src="{{ asset('angular/Admin/UserManagement/Controllers/users-lists.js') }}"></script>
-<script src="{{ asset('angular/Admin/UserManagement/Services/user_management.js') }}"></script>
-<script src="{{ asset('/vendors/sweetalert/dist/sweetalert.min.js')}}"></script>
-<script type="text/javascript">
-  var table = '';
-  $(function() {
-            table = $('#customes-list-table').DataTable({
-      "ajax": {
-            "url" : "getallcostumes",
-           "type": "GET",
-         },
-      "searching": false,
-      "pageLength": 25,
-      "bLengthChange": false,
-      
-      "columns": [
-        { data: 'sku_no', name: 'sku_no'},
-        { data: 'custome_name', name: 'custome_name'},
-        { data: 'customer_name', name: 'customer_name'},
-        { data: 'cat_name', name: 'cat_name'},
-        { data: 'custome_condition', name: 'custome_condition'},
-        { data: 'custome_created_at', name: 'custome_created_at'},
-        { data: 'status', name: 'status'},
-        { data: 'actions', name: 'actions'}
-      ]
-    });
-
-
-
-
-        function deleteinspiringCategorie($id){
-        var id=$id;
-
-    swal({
-       title: "Are you sure want to delete this Category?",
-                  showCancelButton: true,
-                 confirmButtonColor: "#DD6B55 ",
-                 confirmButtonText: "Yes, delete",
-                 closeOnConfirm: false,
-                 closeOnCancel: true
-               },
-
-               function(){
-               url = "/admin/talent/inspiringstories/categories/delete/"+id+"";
-                window.location = url;
-               });
-
-
-   }
-
-  }); 
-   
 
 </script>
 
