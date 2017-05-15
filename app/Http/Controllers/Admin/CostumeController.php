f23ddb2bfd2427d2e1dc7de4f28b3e19689af69b
Mruduramai <mgurrapu@dotcomweavers.com>
2017-05-15 19:36:26 +0530

    changes

--- app/Http/Controllers/Admin/CostumeController.php
+++ app/Http/Controllers/Admin/CostumeController.php
@@ -726,11 +726,10 @@ class CostumeController extends Controller
 		$this->data['costumes_data'] = DB::table('costumes as c')->where('c.costume_id',$id)
 		->leftJoin('users as u','c.created_by','u.id')
 		->leftJoin('costume_description as cd','c.costume_id','cd.costume_id')
-<<<<<<< HEAD
+
 		->select('u.display_name as customer_name','cd.name as costume_name','c.gender as cos_gender','c.condition as cos_condition','c.price as cos_price','c.quantity as cos_quantity','c.size as cos_size','c.charity_id as cos_charity_id','cd.description as cos_description')
-=======
+
 		->select('u.display_name as customer_name','cd.name as costume_name')
->>>>>>> d082dad375e1b5a90d6a9dd2ba37637d02859375
 		->first();
 		$this->data['categories']=array('modules_result'=>array());
 		$this->data['bd_height']=DB::table('attributes')->select('attribute_id','code','label','type')->where('attribute_id','=',16)->first();
