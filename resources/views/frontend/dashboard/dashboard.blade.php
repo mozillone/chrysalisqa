4187e96180b6314f5b5b82f2302ac0f3d0fb904b
V Sudhamsh Mohan <mohan@dotcomweavers.com>
2017-05-18 17:27:17 +0530

    range slider design

--- resources/views/frontend/dashboard/dashboard.blade.php
+++ resources/views/frontend/dashboard/dashboard.blade.php
@@ -12,21 +12,23 @@
  </div>
 </section>    
 	<section class="content create_section_page">
-		<div class="row">
-			<div class="col-md-12 col-sm-12">
-				@if (Session::has('error'))
-	            <div class="alert alert-danger alert-dismissable">
-					<a type="button" class="close" data-dismiss="alert" aria-hidden="true">×</a>
-					{{ Session::get('error') }}
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					@if (Session::has('error'))
+					<div class="alert alert-danger alert-dismissable">
+						<a type="button" class="close" data-dismiss="alert" aria-hidden="true">×</a>
+						{{ Session::get('error') }}
+					</div>
+					@elseif(Session::has('success'))
+					<div class="alert alert-success alert-dismissable">
+						<a type="button" class="close" data-dismiss="alert" aria-hidden="true">×</a>
+						{{ Session::get('success') }}
+					</div>
+					@endif
 				</div>
-	            @elseif(Session::has('success'))
-				<div class="alert alert-success alert-dismissable">
-					<a type="button" class="close" data-dismiss="alert" aria-hidden="true">×</a>
-					{{ Session::get('success') }}
-				</div>
-				@endif
 			</div>
-	</div>
+		</div>
 	
 	<div class="container">
 		<div class="row">
