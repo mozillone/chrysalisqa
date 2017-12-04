cac11eb412b34514146c3be12c07887442cc679c
admin <smannem@dotcomweavers.com>
2017-12-04 13:17:03 +0530

    Debugger Added

--- public_html/assets/frontend/js/pages/costumecustom.js
+++ public_html/assets/frontend/js/pages/costumecustom.js
@@ -442,6 +442,7 @@ $(function(){
                             return false;
                         });
                            $(document).on("input", "#zoom-level", function() {
+                            debugger
                             $image.cropper('zoomTo', 0.1);
                             var current_zoom = $(this).val();
                             $image.cropper('zoom', current_zoom);
