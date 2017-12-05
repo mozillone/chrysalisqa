e8a1ff3d4f32e6ed3da4523fc011c8c19019af2a
gayatri <gbhyri@dotcomweavers.com>
2017-12-05 16:07:38 +0530

    email template changes for admin costume edit

--- resources/views/emails/admineditcostume.blade.php
+++ resources/views/emails/admineditcostume.blade.php
@@ -1,7 +1,7 @@
 @extends('/frontend/email_main')
 @section('content')
 
-<table style="width:100%;font-weight:normal;border-collapse:collapse;border:0;margin:0;padding:0;font-familfffffl,sans-serif;margin-top:0">
+<table style="font-weight:normal;border-collapse:collapse;border:0;padding:0;margin-top:0;width:640px; margin:0 auto;">
 				<tbody>
 					<tr>
 						<td style="border-collapse:collapse;border:0;margin:0;padding:18px;color:#333;font-family:Arial,sans-serif;font-size:16px;line-height:24px;background-color:#fff;">
