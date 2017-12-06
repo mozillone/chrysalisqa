59ab4752f745d9b4b1351bdc8cac824e11723a6e
admin <smannem@dotcomweavers.com>
2017-12-06 13:22:01 +0530

    Image Zoom on IE

--- public_html/assets/frontend/js/pages/costumecustom.js
+++ public_html/assets/frontend/js/pages/costumecustom.js
@@ -441,7 +441,7 @@ $(function(){
                             $("img").mousedown(function(){
                             return false;
                         });
-                           $(document).on("input", "#zoom-level", function() {
+                           $(document).on("click", "#zoom-level", function() {
                             $image.cropper('zoomTo', 0.1);
                             var current_zoom = $(this).val();
                             $image.cropper('zoom', current_zoom);
@@ -524,7 +524,7 @@ $(function(){
                             });
                         }, 1000);
 
-                        $(document).on("input", "#zoom-level2", function() {
+                        $(document).on("click", "#zoom-level2", function() {
                             $image.cropper('zoomTo', 0.1);
                             var current_zoom = $(this).val();
                             $image.cropper('zoom', current_zoom);
@@ -609,7 +609,7 @@ $(function(){
                             });
                         }, 1000);
 
-                        $(document).on("input", "#zoom-level3", function() {
+                        $(document).on("click", "#zoom-level3", function() {
                             $image.cropper('zoomTo', 0.1);
                             var current_zoom = $(this).val();
                             $image.cropper('zoom', current_zoom);
