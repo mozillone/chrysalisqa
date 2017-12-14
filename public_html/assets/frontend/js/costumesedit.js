50f7ee6f53df5a3eb450d8aeb0ad29e00d63be13
admin <smannem@dotcomweavers.com>
2017-12-14 18:45:06 +0530

    CSS Changes

--- public_html/assets/frontend/js/costumesedit.js
+++ public_html/assets/frontend/js/costumesedit.js
@@ -887,7 +887,7 @@ $(document).on("change", "#file2", function() {
                         $(".Backview").attr('value',imgdata);
                         $(".result2").attr("src", imgdata);
                         $("#selected_file_1").remove();
-                        $(".result2").css({ "width": "198px", "height": "298px", "position": "absolute", "top": "-10px","left":"0px" });
+                        $(".result2").css({ "width": "198px", "height": "298px","position": "absolute", "top": "0px","left":"0px"});
                         //$("#file2").hide();
                         $(this).parents().find("#back_view").children("#drag_n_drop_2").removeClass('hide');
                         $('.Backview').attr('data-value',2);
@@ -990,13 +990,13 @@ $(document).on("change", "#file3", function() {
                         $(".drop_zone3").append(Additional);
                         $(".result3").attr("src", imgdata);
                         $("#selected_file_2").remove();
-                        $(".result3").css({ "width": "198px", "height": "298px","position":"relative","bottom":"280px" });
+                        $(".result3").css({ "width": "198px", "height": "298px","position": "absolute", "top": "-10px","left":"0px"});
                         //$("#file3").hide();
                         $(this).parents().find("#details_view").children("#drag_n_drop_3").removeClass('hide');
-                        if($(".drop_zone3").hasClass('additional'))
+                        /*if($(".drop_zone3").hasClass('additional'))
                         {
-                          $(".result3").css({ "width": "198px", "height": "298px","position": "absolute", "top": "0px","left":"0px"});
-                        }
+                          $(".result3").css({ "width": "198px", "height": "298px","position": "absolute", "top": "-10px","left":"0px"});
+                        }*/
                     });
                 };
                 reader.readAsDataURL(file);
