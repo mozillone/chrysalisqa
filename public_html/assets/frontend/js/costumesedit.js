83eea17b6a7f3f39588f2c82ae76c9ceaccdfd52
admin <smannem@dotcomweavers.com>
2017-12-14 19:21:18 +0530

    CSS

--- public_html/assets/frontend/js/costumesedit.js
+++ public_html/assets/frontend/js/costumesedit.js
@@ -887,7 +887,7 @@ $(document).on("change", "#file2", function() {
                         $(".Backview").attr('value',imgdata);
                         $(".result2").attr("src", imgdata);
                         $("#selected_file_1").remove();
-                        $(".result2").css({ "width": "198px", "height": "298px","position": "absolute", "top": "0px","left":"0px"});
+                        $(".result2").css({ "width": "198px", "height": "298px","position": "absolute", "top": "-10px","left":"0px"});
                         //$("#file2").hide();
                         $(this).parents().find("#back_view").children("#drag_n_drop_2").removeClass('hide');
                         $('.Backview').attr('data-value',2);
