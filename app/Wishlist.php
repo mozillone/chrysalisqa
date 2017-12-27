727f44f3d17704b3f9023ccdb04542ebe38d7ba5
sainadhm <smannem@dotcomweavers.com>
2017-12-27 17:39:30 +0530

    Pagination for Favorites

--- app/Wishlist.php
+++ app/Wishlist.php
@@ -8,6 +8,9 @@ use DB;
 use App\Helpers\Site_model;
 use App\Helpers\SiteHelper;
 use Auth;
+use URL;
+use Illuminate\Support\Facades\Input;
+use Request;
 
 class Wishlist extends Authenticatable
 {
@@ -28,6 +31,12 @@ class Wishlist extends Authenticatable
     protected function myWishlistList($user_id){
         //echo $user_id; exit;
         $wish_list=DB::Select('SELECT  cst.costume_id,cst.gender,cst.size,dsr.name,CONCAT("$",FORMAT(cst.price,2)) as price,if((select count(*) from cc_costumes_like as likes where likes.user_id='.Auth::user()->id.' and  likes.costume_id=cst.costume_id )>=1,true,false) as is_like,if((select count(*) from cc_customer_wishlist as wsh_lst where wsh_lst.user_id='.Auth::user()->id.' and  wsh_lst.  costume_id=cst.costume_id )>=1,true,false) as is_fav,(SELECT count(*) FROM `cc_costumes_like` where costume_id=cst.costume_id) as like_count,img.image,link.url_key,cst.quantity FROM `cc_customer_wishlist` as wish LEFT JOIN cc_costumes as cst on cst.costume_id=wish.costume_id  LEFT JOIN cc_costume_image as img on img.costume_id=cst.costume_id and img.type="1"  LEFT JOIN cc_costume_description as dsr on dsr.costume_id=cst.costume_id LEFT JOIN cc_url_rewrites as link on link.url_offset=cst.costume_id and link.type="product" where cst.deleted_status=0 and wish.user_id='.$user_id.' group by cst.costume_id');
+        $page = Input::get('page', 1);
+        $paginate = 10;
+        $offSet = ($page * $paginate) - $paginate;
+        $itemsForCurrentPage = array_slice($wish_list, $offSet, $paginate, true);
+        $wish_list = new \Illuminate\Pagination\LengthAwarePaginator($itemsForCurrentPage, count($wish_list), $paginate, $page);
+        $wish_list = $wish_list->setPath(Request::Url());
        // echo "<pre>"; print_r($wish_list); exit;
         return $wish_list;
     }
