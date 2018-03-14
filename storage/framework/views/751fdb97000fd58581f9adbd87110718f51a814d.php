0b7d5bab6cdf49b0ca06a70268f8ce0f0dbe6ea0
gayatri <gbhyri@dotcomweavers.com>
2018-03-14 16:54:03 +0530

    master commit

--- storage/framework/views/751fdb97000fd58581f9adbd87110718f51a814d.php
+++ storage/framework/views/751fdb97000fd58581f9adbd87110718f51a814d.php
@@ -7,7 +7,15 @@
 				<div class="col-md-12">
 					<div class="icon_lins text-right">
 						<ul> 
-							<li><a href="<?php echo e(URL::to('conversations')); ?>" type="button" class="btn btn-default btn-lg text-center"><i class="fa fa-envelope" aria-hidden="true"></i><br>Messages</a></li>
+							<li>
+								<a href="<?php echo e(URL::to('conversations')); ?>" type="button" class="btn btn-default btn-lg text-center">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+									<?php if(Auth::check()): ?>
+										<span class="msg_count"><?php echo e(helper::getMyMessageCount()); ?></span>
+									<?php endif; ?>
+									<br>Messages
+								</a>
+							</li>
 							<li><a type="button" class="btn btn-default btn-lg text-center fav-icon-sec" href="<?php echo e(route('wishlist')); ?>"><i class="fa fa-heart" aria-hidden="true"></i><?php if(Auth::check()): ?><span class="fav_count"><?php echo e(helper::getMyWishlistCount()); ?></span><?php endif; ?><br><?php if(Auth::check()): ?><a class="fav-style" href="<?php echo e(route('wishlist')); ?>">Favorites</a> <?php else: ?> <a class="fav-style" data-toggle="modal" data-target="#login_popup"> Favorites </a> <?php endif; ?> </a></li>
 							<li><button type="button" class="dropdown-toggle btn btn-default btn-lg text-center mini-cart" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-shopping-cart" aria-hidden="true"></i><br>Cart <span class="mini_cart"><?php if(is_numeric(helper::getCartCount())): ?><?php echo e(helper::getCartCount()); ?> <?php else: ?> 0 <?php endif; ?></span></button>
 								<ul class="dropdown-menu cart-products">
@@ -118,7 +126,7 @@
 	<section class="responsive-menu hidden-lg  hidden-md sticky-head">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-5">
+				<div class="col-xs-4">
 					<div class="icon-rm"><span class="toggle-btn">
 						<span class="btn-line"></span>
 						<span class="btn-line"></span>
@@ -128,19 +136,19 @@
 						<a href="/"><img class="img-responsive" src="<?php echo e(asset('img/brand.png')); ?>"></a>
 					</div>
 				</div>
-				<div class="col-xs-7">
+				<div class="col-xs-8">
 					<a href="/cart" type="button" class="navbar-toggle respnsive-ser-rm sell mbl_crt_icon" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
 						<i class="fa fa-shopping-cart" aria-hidden="true"></i>
 					</a>
-					<button type="button" class="navbar-toggle respnsive-ser-rm" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
+					<span type="button" class="navbar-toggle respnsive-ser-rm" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
 						<a href="<?php echo e(URL::to('conversations')); ?>" type="button" class=""><i class="fa fa-envelope" aria-hidden="true"></i></a>
-					</button>
-					<button type="button" class="navbar-toggle respnsive-ser-rm" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
+					</span>
+					<span type="button" class="navbar-toggle respnsive-ser-rm" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
 						<a data-toggle="modal" <?php if(!Auth::check()): ?> data-target="#login_popup" <?php else: ?> href="/dashboard" <?php endif; ?>><i class="fa fa-user" aria-hidden="true"></i></a>
-					</button>
-					<button type="button" class="navbar-toggle respnsive-ser-rm mb-searchs" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
+					</span>
+					<span type="button" class="navbar-toggle respnsive-ser-rm mb-searchs" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
 						<i class="fa fa-search"></i>
-					</button>
+					</span>
 					<a href="<?php echo e(URL::to('costume/sell-a-costume')); ?>" type="button" class="navbar-toggle respnsive-ser-rm sell" data-toggle="collapse" data-target=".nav-search" data-collapse-group="myDivs">
 						<i class="fa fa-tag" aria-hidden="true"><span>Sell</span></i>
 					</a>
@@ -207,7 +215,7 @@
 							<!-- tab content starts -->			  
 							<div class="head-acc-form">
 								<p class="acc-form-rm"><a href="javascript::void(0);"><input type="text" placeholder="<?php echo e(Auth::user()->display_name); ?>"></a><span class="acc-form-icn"><i class="fa fa-user" aria-hidden="true"></i></span></p>
-								<p class="acc-form-rm"><a href="<?php echo e(route('wishlist')); ?>"><input type="text" placeholder="FAVORITES"></a><span class="acc-form-icn"><i class="fa fa-heart" aria-hidden="true"></i><?php echo e(helper::getMyWishlistCount()); ?></span></p>			
+								<p class="acc-form-rm"><a href="<?php echo e(route('wishlist')); ?>"><input type="text" placeholder="FAVORITES"></a><span class="acc-form-icn"><i class="fa fa-heart" aria-hidden="true"></i><?php echo e(helper::getMyWishlistCount()); ?></span></p>		
 								<p class="acc-form-rm"><a href="<?php echo e(URL::to('conversations')); ?>"><input type="text" placeholder="MESSAGES"></a><span class="acc-form-icn"><i class="fa fa-envelope" aria-hidden="true"></i> <?php echo e(helper::getMessagesCount()); ?></span></p>						
 								<p class="acc-form-rm"><a href="<?php echo e(route('logout')); ?>"><input type="text" placeholder="SIGN OUT"> </a><span class="acc-form-icn"><i class="fa fa-sign-out" aria-hidden="true"></i></span></p>			
 							</div>
