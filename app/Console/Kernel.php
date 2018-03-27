9b5c06b76d6a1d4a18d19e430bbf21db67c3b824
gayatri <gbhyri@dotcomweavers.com>
2018-03-27 15:26:40 +0530

    kernal file changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('clean:cache')->everyMinute();
+        
         $schedule->exec(base_path().'composer dump-autoload')->everyMinute();
     }
 
